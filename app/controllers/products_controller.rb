b3cca93ee02af5291e5c3f98e478b61244eab8d2
bryon burke <bryonburke@bryons-MBP.hsd1.or.comcast.net>
2020-01-18 14:01:54 -0800

    project views

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,42 +1,51 @@
 class ProjectsController < ApplicationController
 
   def index
-     @albums = Album.all
-     render :index
-   end
+    @projects = Project.all
+    render :index
+  end
 
-   def new
-     @album = Album.new
-     render :new
-   end
+  def new
+    @project = Project.new
+    render :new
+  end
 
   def create
-    @album = Album.new(album_params)
-    if @album.save
-      redirect_to albums_path
+    @project = Project.new(project_params)
+    if @project.save
+      redirect_to projects_path
     else
       render :new
     end  end
 
-  def edit
-    # Code for edit album form goes here.
-  end
+    def edit
+      @project = Project.find(params[:id])
+      render :edit
+    end
 
-  def show
-    # Code for showing a single album goes here.
-  end
+    def show
+      @project = Project.find(params[:id])
+      render :show
+    end
 
-  def update
-    # Code for updating an album goes here.
-  end
+    def update
+      @project= Project.find(params[:id])
+      if @project.update(project_params)
+        redirect_to projects_path
+      else
+        render :edit
+      end
+    end
 
-  def destroy
-    # Code for deleting an album goes here.
-  end
+    def destroy
+      @project = Project.find(params[:id])
+      @project.destroy
+      redirect_to projects_path
+    end
 
-  private
-  def album_params
-    params.require(:album).permit(:name, :genre)
-  end
+    private
+    def project_params
+      params.require(:project).permit(:name, :genre)
+    end
 
-end
+  end

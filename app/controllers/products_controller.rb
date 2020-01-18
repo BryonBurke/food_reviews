79235fa3846f0932f755eb6ae61a72d598459e78
bryon burke <bryonburke@bryons-MBP.hsd1.or.comcast.net>
2020-01-18 15:35:14 -0800

    edit misspelled words

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,51 +1,51 @@
-class ProjectsController < ApplicationController
+class ProductsController < ApplicationController
 
   def index
-    @projects = Project.all
+    @products = Product.all
     render :index
   end
 
   def new
-    @project = Project.new
+    @product = Product.new
     render :new
   end
 
   def create
-    @project = Project.new(project_params)
-    if @project.save
-      redirect_to projects_path
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path
     else
       render :new
     end  end
 
     def edit
-      @project = Project.find(params[:id])
+      @product = Product.find(params[:id])
       render :edit
     end
 
     def show
-      @project = Project.find(params[:id])
+      @product = Product.find(params[:id])
       render :show
     end
 
     def update
-      @project= Project.find(params[:id])
-      if @project.update(project_params)
-        redirect_to projects_path
+      @product= Product.find(params[:id])
+      if @product.update(product_params)
+        redirect_to products_path
       else
         render :edit
       end
     end
 
     def destroy
-      @project = Project.find(params[:id])
-      @project.destroy
-      redirect_to projects_path
+      @product = Product.find(params[:id])
+      @product.destroy
+      redirect_to products_path
     end
 
     private
-    def project_params
-      params.require(:project).permit(:name, :genre)
+    def product_params
+      params.require(:product).permit(:name, :genre)
     end
 
   end

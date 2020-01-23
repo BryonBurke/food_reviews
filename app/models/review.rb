6df1ac916dd1e1a6a25490aae7755617dd11a894
bryon burke <bryonburke@bryons-MacBook-Pro.local>
2020-01-22 20:28:56 -0800

    work on validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,6 @@ class Review < ApplicationRecord
   belongs_to :product
 
   validates :author, presence: true
-  validates_length_of :name, minimum: 50 maximum: 250 
+
   validates :product_id, presence: true
 end

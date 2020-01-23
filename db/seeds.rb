0a150e0cbce0a1894863ada448e62c38c9f24362
bryon burke <bryonburke@bryons-MacBook-Pro.local>
2020-01-22 19:41:18 -0800

    add faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.destroy_all
+
+50.times do |index|
+  Product.create!(name: Faker::Food.dish,
+                        cost: Faker::Number.decimal(l_digits: 2),
+                        country_of_origin: Faker::Nation.nationality)
+                        # image_url: "site.com/here_is_a_picture_of_cardamom.jpg")
+end

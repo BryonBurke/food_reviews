cf5a0cfb123d716221082c22089955409333e086
bryon burke <bryonburke@bryons-MacBook-Pro.local>
2020-01-22 20:18:57 -0800

    add validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :product
+
+  validates :author, presence: true
+  validates_length_of :name, minimum: 50 maximum: 250 
+  validates :product_id, presence: true
 end
